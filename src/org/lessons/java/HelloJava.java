98082e30f3dbf52706a80044d86c8f9a084d27f7
marcantonio99 <marcantonio8799@gmail.com>
2023-05-30 16:18:52 +0200

    rename package

--- src/org/lessons/java/HelloJava.java
+++ src/org/lessons/java/HelloJava.java
@@ -1,7 +1,8 @@
-package org.learning.java;
+package org.lessons.java;
 
 public class HelloJava {
     public static void main(String[] args) {
+
         System.out.println("Hello Java");
     }
 }
